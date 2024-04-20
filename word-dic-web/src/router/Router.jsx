9ffcbba2678fd274c02667daf0e7829e4a73ffed
jeong jun <jungjun3047@naver.com>
2024-04-20 19:09:31 +0900

    fix: electron 배포 문제로 HOME을 기본으로

--- word-dic-web/src/router/Router.jsx
+++ word-dic-web/src/router/Router.jsx
@@ -6,6 +6,10 @@ import PrivateRoute from './PrivateRoute'
 import LogoutRoute from './LogoutRoute'
 
 const Router = () => {
+
+    // electron 배포 문제로 HOME을 기본으로
+    return <Home />
+
     return (
       <BrowserRouter>
           <Routes>
