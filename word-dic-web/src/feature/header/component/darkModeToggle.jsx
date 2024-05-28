d89c8afb4495c6b375808dc80b8f6d7be296e8dc
jeong jun <jungjun3047@naver.com>
2024-05-28 15:03:55 +0900

    chore: 불필요한 부분 제거

--- word-dic-web/src/feature/header/component/darkModeToggle.jsx
+++ word-dic-web/src/feature/header/component/darkModeToggle.jsx
@@ -9,17 +9,14 @@ const DarkModeToggle = () => {
     return (
         <button
           onClick={toggleDarkMode}
-          className={`hidden md:flex transition-colors duration-300 absolute items-center justify-center border-1 rounded-full gap-17r right-120r w-100r h-46r py-5r px-8r
-          ${isDarkMode ? 'bg-bg border-c-grey-60' : 'bg-white border-c-grey-50'}`}
+          className={`hidden md:flex transition-colors duration-300 absolute items-center justify-center border-1 rounded-full gap-17r right-120r w-100r h-46r py-5r px-8r dark:bg-bg dark:border-c-grey-60 bg-white border-c-grey-50`}
         >
-          <div className={`absolute rounded-full w-36r h-36r transition-transform-colors duration-300
-            ${isDarkMode ? 'right-10r bg-c-grey-60' : 'right-51r bg-c-grey-40'}`}
-          />
+          <div className={`absolute rounded-full w-36r h-36r transition-transform-colors duration-300 dark:right-10r dark:bg-c-grey-60 right-51r bg-c-grey-40`} />
             <SunIcon
-                className={`w-24r h-24r z-10 transition-colors duration-300 ${isDarkMode ? 'text-c-grey-60' : 'text-c-grey-90'}`}
+                className={`w-24r h-24r z-10 transition-colors duration-300 dark:text-c-grey-60 text-c-grey-90`}
             />
             <MoonIcon
-                className={`w-24r h-24r z-10 transition-colors duration-300 ${isDarkMode ? 'text-c-grey-20' : 'text-c-grey-60'}`}
+                className={`w-24r h-24r z-10 transition-colors duration-300 dark:text-c-grey-20 text-c-grey-60`}
             />
         </button>
       );    
--- word-dic-web/src/feature/header/hook/useDarkMode.js
+++ word-dic-web/src/feature/header/hook/useDarkMode.js
@@ -7,28 +7,10 @@ const useDarkMode = () => {
   const toggleDarkMode = useDarkModeStore((state) => state.toggleDarkMode);
 
   useEffect(() => {
-    const metaThemeColor = document.querySelector('meta[name="theme-color"]');
-
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
-      if (metaThemeColor) {
-        metaThemeColor.setAttribute('content', '#000000');
-      } else {
-        const meta = document.createElement('meta');
-        meta.name = 'theme-color';
-        meta.content = '#000000';
-        document.head.appendChild(meta);
-      }
     } else {
       document.documentElement.classList.remove('dark');
-      if (metaThemeColor) {
-        metaThemeColor.setAttribute('content', '#ffffff');
-      } else {
-        const meta = document.createElement('meta');
-        meta.name = 'theme-color';
-        meta.content = '#ffffff';
-        document.head.appendChild(meta);
-      }
     }
   }, [isDarkMode]);
 
